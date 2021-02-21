4074aab1a5fa5e14467739ded4cb2748a3415c29
Gustavo Domingueti <gustav.s.domingueti@gmail.com>
2021-02-21 11:45:47 -0300

    Install angular-calendar and flatpickr and configure initial calendar structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, AngularMaterialModule, FlexLayoutModule],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, AngularMaterialModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+
+    AngularMaterialModule,
+
+    FlexLayoutModule,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    FlatpickrModule,
+    CalendarModule,
+
+    AngularMaterialModule,
+
+    FlexLayoutModule,
+  ],
 })
 export class SharedModule {}
