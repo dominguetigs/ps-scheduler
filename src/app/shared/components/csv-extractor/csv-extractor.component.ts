e49fa4493a1f1e94a67f0221188c55809dd202f1
Gustavo Domingueti <gustav.s.domingueti@gmail.com>
2021-02-22 01:56:19 -0300

    Adjustments in datepicker and validations

--- src/app/shared/components/csv-extractor/csv-extractor.component.ts
+++ src/app/shared/components/csv-extractor/csv-extractor.component.ts
@@ -34,7 +34,7 @@ export class CSVExtractorComponent implements OnInit, OnDestroy {
   // -----------------------------------------------------------------------------------------------------------------
 
   ngOnInit(): void {
-    this.isExtractMode = !this.items.length;
+    this.isExtractMode = !this?.items?.length;
   }
 
   ngOnDestroy(): void {
