505256d2ff3ea18918aa810850bbb13634162d9f
Gustavo Domingueti <gustav.s.domingueti@gmail.com>
2021-02-23 23:55:50 -0300

    Create font size generator mixin

--- src/app/core/template/header/header.component.ts
+++ src/app/core/template/header/header.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 
 import { SidenavService } from '../sidenav/sidenav.service';
 
+import { MENU_ITEMS } from './profile-menu-items.constant';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  menuItems = MENU_ITEMS;
+
   constructor(private _sidenavService: SidenavService) {}
 
   // -----------------------------------------------------------------------------------------------------------------
---
+++ src/app/core/template/header/profile-menu-items.constant.ts
@@ -0,0 +1,14 @@
+export const MENU_ITEMS = [
+  {
+    icon: 'person',
+    label: 'Profile',
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+  },
+  {
+    icon: 'logout',
+    label: 'Logout',
+  },
+];
